073465b628494d990ca87dff3686d8f8b3a89f02
Melih Erduran <merduran.ed@gmail.com>
2022-08-30 12:59:01 -0700

    Learning

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,2 +1,19 @@
-// This is my first JavaScript Code
 console.log('Hello World');
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+let firstName = 'Melih';
+let lastName = 'Erduran';
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+let name = 'Melih'; // String Literal
+let age = 30; // Number Literal
+let isApproved = false; // Boolean Literal
+firstName = undefined;
+let selectedColor = null;
