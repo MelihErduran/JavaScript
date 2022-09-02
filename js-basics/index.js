cc6a6d5b83790dd1202a576a37f6b8cbdf6e080e
Melih Erduran <merduran.ed@gmail.com>
2022-09-02 12:58:28 -0700

    Learning objects

--- js-basics/index.js
+++ js-basics/index.js
@@ -12,8 +12,21 @@ const interestRate = 0.3;
 interestRate = 1;
 console.log(interestRate);
 
-let name = 'Melih'; // String Literal
+/*let name = 'Melih'; // String Literal
 let age = 30; // Number Literal
 let isApproved = false; // Boolean Literal
 firstName = undefined;
-let selectedColor = null;
+let selectedColor = null; */
+let person = {
+    name: 'Melih',
+    age: 15,
+};
+
+console.log(person);
+
+person.name = 'Melih2';
+person['name'] = 'Melih3';
+
+let selection = 'name';
+person[selection] = 'Melih4';
+
