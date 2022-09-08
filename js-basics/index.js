27dee2b6af1bce71d53ff2cc2ee64c15f05c3142
Melih Erduran <merduran.ed@gmail.com>
2022-09-08 12:57:26 -0700

    Finished Course 1

--- js-basics/index.js
+++ js-basics/index.js
@@ -32,3 +32,11 @@ person[selection] = 'Melih4';
 let selectedColors = ['red', 'blue'];
 selectedColors[2] = 1;
 console.log(selectedColors.length)
+function greet(name){
+    console.log(name + ' is the best programmer')
+};
+greet('Melih Erduran');
+
+function triArea(base, height){
+    return((base * height)/2)
+}
