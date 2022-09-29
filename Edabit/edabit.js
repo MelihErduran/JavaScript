f70a673a183b6b7811925240e16c25a2c936b73a
Melih Erduran <merduran.ed@gmail.com>
2022-09-29 12:59:54 -0700

    Finihsed another challenge

--- Edabit/edabit.js
+++ Edabit/edabit.js
@@ -6,3 +6,11 @@ function minMax(arr) {
 }
 
 minMax([1,3,5]);
+
+function addUp(num1, num2) {
+    let total = 0
+    for (var i=num1; i<=num2; i++){
+        total += i;
+    }
+    return(total)
+}
